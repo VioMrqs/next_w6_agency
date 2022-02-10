63dc4acc24c222a0b42ce15d480827ec6ed115e2
VioMrqs <violette.marquis@gmail.com>
2022-02-10 10:33:27 +0100

    button

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,34 +1,38 @@
 import { Link } from "react-router-dom";
 import Button from "../Button"
+import {FaHome} from 'react-icons/fa'
 import { useContext } from "react";
 import { ThemeContext } from "../../ThemeContext";
 
 const Navbar = () => {
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+  const style = { fontSize: "1.5em" };
 
   return (
-  <div className="navbar">
-    <div className="navbar-right" >
-      <ul>
-        <Link to="/">Accueil</Link>
-      </ul>
-      <ul>
-        <Link to="/about">A propos</Link>
-      </ul>
-      <ul>
-        <Link to="/works">Projets</Link>
-      </ul>
+    <div className="navbar">
+      <div className="navbar-right">
+        <ul>
+          <Link to="/">
+            <FaHome style={style} />
+          </Link>
+        </ul>
+        <ul>
+          <Link to="/about">A propos</Link>
+        </ul>
+        <ul>
+          <Link to="/works">Projets</Link>
+        </ul>
+      </div>
+      <div className="navbar-left">
+        <ul>
+          <Button
+            onClick={toggleDarkMode}
+            text={darkMode ? "Jour" : "Nuit"}
+            className={darkMode ? "button button-light" : "button button-dark"}
+          />
+        </ul>
+      </div>
     </div>
-    <div className="navbar-left">
-      <ul>
-        <Button
-          onClick={toggleDarkMode}
-          text={darkMode ? "Jour" : "Nuit"}
-          className={darkMode ? "button button-light" : "button button-dark"}
-        />
-      </ul>
-    </div>
-  </div>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
 const Home = () => {
+
 return (
   <div>
-    <h1>Concepteur des internets</h1>
+    <h2>This is not a web agency</h2>
+    <h1>C'est un concepteur des internets</h1>
     <p>
       Grâce à notre savoir-faire, notre expérience et notre écoute, nous
       accompagnons nos clients dans la création de site internet: étude, UX,
-      conception, design, développement, SEO. Notre agence web est capable de
-      répondre à tous vos besoins et d'élaborer une véritable stratégie
-      digitale.
+      conception, design, développement, SEO, CEO et j'en passe.
+    </p>
+    <p>
+      Notre agence web est capable de répondre à tous vos besoins et d'élaborer
+      une véritable stratégie digitale.
     </p>
   </div>
 );}
